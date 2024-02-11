ea6445fe211ec5dc0f117e029bcaaebc17bbd870
Divyesh Godhani <godhanidivyesh44@gmail.com>
2024-02-11 10:08:21 +0530

    changes whatsapp and sms

--- src/app/navigations.js
+++ src/app/navigations.js
@@ -1,16 +1,6 @@
 export const navigations = [
   { name: 'Dashboard', path: '/dashboard/default', icon: 'dashboard' },
   {
-    name: 'SMS',
-    icon: 'sms',
-    children: [
-      { name: 'Sign in', iconText: 'SI', path: '/session/signin' },
-      { name: 'Sign up', iconText: 'SU', path: '/session/signup' },
-      { name: 'Forgot Password', iconText: 'FP', path: '/session/forgot-password' },
-      { name: 'Error', iconText: '404', path: '/session/404' }
-    ]
-  },
-  {
     name: 'WhatsApp',
     icon: 'message',
     children: [
@@ -28,6 +18,16 @@ export const navigations = [
       { name: 'Slider', path: '/material/slider', iconText: 'S' },
       { name: 'Snackbar', path: '/material/snackbar', iconText: 'S' },
       { name: 'Table', path: '/material/table', iconText: 'T' }
+    ]
+  },
+  {
+    name: 'SMS',
+    icon: 'sms',
+    children: [
+      { name: 'Sign in', iconText: 'SI', path: '/session/signin' },
+      { name: 'Sign up', iconText: 'SU', path: '/session/signup' },
+      { name: 'Forgot Password', iconText: 'FP', path: '/session/forgot-password' },
+      { name: 'Error', iconText: '404', path: '/session/404' }
     ]
   },
   {
